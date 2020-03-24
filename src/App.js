573319e84c78f802ab828c0c73b79b4dd81f5672
Samer Senbol <samer.senbol@gmail.com>
2020-03-24 14:30:17 +0100

    Passing the App state as props to todos component

--- src/App.js
+++ src/App.js
@@ -25,10 +25,9 @@ export class App extends React.Component{
     ]
   }
   render(){
-    console.log(this.state.todos) 
     return(
     <div className="App">
-      <Todos />
+      <Todos todos={this.state.todos}/>
     </div>
     );
   }
