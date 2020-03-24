ca8376dd0bcf304fff41703a0053654a8647d827
Samer Senbol <samer.senbol@gmail.com>
2020-03-24 14:11:19 +0100

    Creating state in App component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React from 'react';
 import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  return (
+
+export class App extends React.Component{
+
+  state = {
+    todos: [
+      {
+        id:1,
+        title:'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title:'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title:'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render(){
+    console.log(this.state.todos) 
+    return(
     <div className="App">
       <Todos />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+
+export class Todos extends React.Component{
+    render(){
+      return(
+        <div>
+            <h1>Todos</h1>
+        </div>
+      );
+    }
 }
 
 export default Todos;
