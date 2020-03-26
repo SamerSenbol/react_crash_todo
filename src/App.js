759099f55b396245809832d84794d2f2b37482b4
Samer Senbol <samer.senbol@gmail.com>
2020-03-26 15:18:31 +0100

    Adding header and addTodo form with their style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 
@@ -42,7 +44,12 @@ export class App extends React.Component{
   render(){
     return(
     <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <div className="container">
+        <Header />
+        <AddTodo />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
+      
     </div>
     );
   }
