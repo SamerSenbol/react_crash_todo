e7821e96894679b37bbf4bc06e648777da28dee4
Samer Senbol <samer.senbol@gmail.com>
2020-03-27 09:40:47 +0100

    Running addTodo function via props by changing state when calling onSubmit and onChange events

--- src/App.js
+++ src/App.js
@@ -41,12 +41,21 @@ export class App extends React.Component{
   delTodo = (id) => {
     this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
+  }
   render(){
     return(
     <div className="App">
       <div className="container">
         <Header />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo}/>
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
       
