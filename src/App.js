b6a418d152a1075c41c6a92d36a350f53c392e1b
Samer Senbol <samer.senbol@gmail.com>
2020-03-27 13:53:01 +0100

    Transferring delete and add Item methods to axios request and respond methods

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import * as uuid from "uuid";
+//import * as uuid from "uuid";
 import axios from 'axios';
 import './App.css';
 
@@ -32,16 +32,18 @@ export class App extends React.Component{
 
   //Delete Todo
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]}))
+    
   }
   // Add Todo
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
+    axios.post('https://jsonplaceholder.typicode.com/todos',{
       title,
       completed: false
-    }
-    this.setState({todos: [...this.state.todos, newTodo]})
+    })
+    .then(res=>  this.setState({todos: [...this.state.todos, res.data]}));
+   
   }
   render(){
     return(
